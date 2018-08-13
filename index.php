a4ccbc2db3d487b48d1530d64cdcd634b10bb2e2
2244yen <linhdtt92@gmail.com>
2018-08-13 10:39:18 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,8 +1,7 @@
 <?php
-    session_start();
-
     include_once 'login.php';
-    include_once 'get_page_access_token.php';
+
+    session_start();
 
     error_reporting(E_ALL);
     ini_set("display_errors", 0);
